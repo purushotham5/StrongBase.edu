17824c6ed2c4ba4b12aa604c801e46d309e5f577
purushotham5 <purushothamkandheri@gmail.com>
2023-12-03 22:14:43 +0530

    ad

--- job-portal.js
+++ job-portal.js
@@ -4,29 +4,29 @@ const jobs = [
     image: "software-engineer.svg",
     skills: "Skills:Java Android,HTML,CSS,JS",
     details:
-      "We are hiring for Freshers Java Net Android Web Developer Qualification B Tech B E M C A Experience 0 4Years Skills Fresher Java Android IOS Developer Javascript Jquery CSS HTML PHP Candidate Profile Candidate should have 1 Strong programming skills 2 Excellent problemsolving skills 3 Excellent communication skills 4 Good to have knowledge in Javascript Jquery CSS HTML PHP etc Contact Mr Vasanth hidden mobile",
-    openPositions: "2",
+      "We are hiring for Freshers Java Net Android Web Developer <br> Qualification B Tech B E M C A Experience 0 4Years Skills Fresher.",
+    openPositions: "Fresher",
     link: "https://in.jobrapido.com/jobpreview/261754190?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic",
   },
 
   {
     title: "Software Engineer",
-    image: "software-engineer.svg",  
-      skills: "Skills:Java,SQL,Python,HTML,CSS,JS",
+    image: "software-engineer.svg",
+    skills: "Skills:Java,SQL,Python,HTML,CSS,JS",
     details:
       "Responsible for collecting, analyzing and interpreting large data sets to help organizations make better decisions.",
-    openPositions: "3",
-    link: "https://www.shine.com/jobs/software-engineer-fresher/covai-industrials-recruitment/12778956?utm_source=linkedin-feed&amp;utm_medium=referral&amp;utm_campaign=linkedin-feed&amp;auto_apply=true",
+      openPositions: "Fresher",
+      link: "https://www.shine.com/jobs/software-engineer-fresher/covai-industrials-recruitment/12778956?utm_source=linkedin-feed&amp;utm_medium=referral&amp;utm_campaign=linkedin-feed&amp;auto_apply=true",
   },
 
-   {
+  {
     title: "Software Engineer",
-    image: "software-engineer.svg",  
-      skills: "Skills:Java,SQL,Python,HTML,CSS,JS",
+    image: "software-engineer.svg",
+    skills: "Skills:Java,SQL,Python,HTML,CSS,JS",
     details:
       "We are hiring for Freshers Java Net Android Web Developer Qualification B Tech B E M C A.",
-    openPositions: "3",
-    link: "https://www.linkedin.com/jobs/view/software-engineer-fresher-at-covai-industrials-recruitment-3751442332/?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic&originalSubdomain=in",
+      openPositions: "Fresher",
+      link: "https://www.linkedin.com/jobs/view/software-engineer-fresher-at-covai-industrials-recruitment-3751442332/?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic&originalSubdomain=in",
   },
 
   {
@@ -35,52 +35,80 @@ const jobs = [
     skills: "Skills:Python,SQL",
     details:
       "We are looking for a Python freshers to join our team and help us develop and maintain various software products.Freshers only need to apply Also providing placement assistence after internship period on the basis of performance of work.",
-    openPositions: "1",
-    link: "https://www.simplyhired.co.in/job/daKacOiPFIA11R3aJhnFZTDpdC4xn4VIeO13so0YAeih8Q8SqRprIQ?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic",
+      openPositions: "Fresher",
+      link: "https://www.simplyhired.co.in/job/daKacOiPFIA11R3aJhnFZTDpdC4xn4VIeO13so0YAeih8Q8SqRprIQ?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic",
   },
 
   {
-    title: "Product Manager",
+    title: "HTML / XML Programmer",
     image: "product-manager.svg",
+    skills: "Skills:HTML,XML",
     details:
-      "Responsible for managing the entire product life cycle, from ideation to launch and post-launch maintenance.",
-    openPositions: "1",
-    link: "#",
+      "ResponsWe are hiring HTML / XML Programmer in Tirupati with some basic exposer wring content online which can add value to the brand, ideal candidate should be graduated from UGC approved campus and ready to boost their career into development.",
+      openPositions: "Fresher",
+      link: "https://freshersjobsalert.com/career/fresher-html-xml-programmer-jobs-in-tirupati-andhra-pradesh?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic",
   },
 
   {
-    title: "Sales Representative",
+    title: "Work From Home/Computer Operator work",
+    image: "sales-representative.svg",    skills: "Skills: Team Leadership, Computer Literacy",
+
+    details:
+      "We are offering a part-time Permanent Job... urgently required candidates for data entry operators It's an global opportunity It is Part time work, full time work..anyone can apply Work from smartphone or laptop or by any gadgets can be done....      ",
+      openPositions: "Fresher",
+      link: "https://www.linkedin.com/jobs/view/work-from-home-computer-operator-work-fresher-experience-at-aspect-solution-3752097936/?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic&originalSubdomain=in",
+  },
+
+  {
+    title: "Software Engineer I",
+    image: "software-engineer.svg",
+    skills:"Java, JavaScript, NodeJs, SpringBoot, Angular, React",
+    details:
+      "Programming knowledge of Java, JavaScript, NodeJs, SpringBoot, Angular, React, or competitive technologies",
+      openPositions: "Fresher",
+      link: "https://alliance.wd3.myworkdayjobs.com/en-US/nissanjobs/job/software-engineer-i_r00151957-1",
+  },
+  {
+    title: "JAVA Developer Internship",
     image: "sales-representative.svg",
+    skills:'BSc in Computer Science, Engineering, or relevant field Understanding of Github version control',
     details:
-      "Responsible for reaching out to potential customers and closing sales deals.",
-    openPositions: "4",
-    link: "#",
+      "Designing and developing high-volume, low-latency applications for mission-critical systems and delivering high-availability and performance. ",
+      openPositions: "Internship",
+      link: "https://www.linkedin.com/jobs/view/3667474965/?alternateChannel=search&refId=b4ed72a2-b021-499a-bb8f-fbfc95cdf26a&trackingId=lVFpV%2BduQj2bf0WdCNtTkw%3D%3D",
   },
 
   {
-    title: "Marketing Manager",
+    title: "Java Programmer - Fresher",
     image: "marketing-manager.svg",
+    skills:"Strong Knowledge in Java,Fluent in English, Good Communication skills",
     details:
-      "Responsible for creating and executing marketing strategies to promote a company or product.",
-    openPositions: "1",
-    link: "#",
-  },
-  {
-    title: "Front-End Developer",
-    image: "sales-representative.svg",
+      "developing high-volume, low-latency applications for mission-critical systems.....",
+      openPositions: "Fresher",
+      link: "https://www.simplyhired.co.in/job/6Zqg2_aeB8jIneXhXyyRH3T4lLV-LHTVefHgUuGVl1iNI3f0yH3j9g?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic",
+  }, {
+    title: "Web Development Intern",
+    image: "product-manager.svg",
+    skills:'HTML,CSS,JS (Basics)',
     details:
-      "Responsible for reaching out to potential customers and closing sales deals.",
-    openPositions: "40",
-    link: "#",
-  },
-
-  {
-    title: "Backend Developer",
-    image: "marketing-manager.svg",
+      "We needs new age interns to become a good world generation",
+      openPositions: "Internship",
+      link: "https://www.linkedin.com/jobs/view/3777298113/?alternateChannel=search&refId=hLOHAGy60CjKSjGFn%2B6LUg%3D%3D&trackingId=wkbmGm0y3skuc6iCFrwGkQ%3D%3D",
+  },{
+    title: "Product Management Intern    ",
+    image: "sales-representative.svg",    skills: "Skills: Team Leadership, Computer Literacy,Microsoft Excel, Analytical Skills, +8 more",
     details:
-      "Responsible for creating and executing marketing strategies to promote a company or product.",
-    openPositions: "1",
-    link: "#",
+      ". This is an internship role, and the selected candidate will work closely with the team and learn about product strategy, market research, and financial product management.....      ",
+      openPositions: "Internship",
+      link: "https://www.linkedin.com/jobs/view/3773061191/?alternateChannel=search&refId=R6bi8F4Df4U%2BOQMQhycVig%3D%3D&trackingId=SAhvQuqgb9YnbG%2B%2BSfGQdg%3D%3D#HYM",
+  },{
+    title: "Winter Training and Internship with Projects",
+    image: "software-engineer.svg",
+    skills:"Coaching, Employee Training, Human Resources (HR), Marketing, Research Skills, Team Work ",
+    details:
+      "As a ParParticipate in 3 real time projects(2 capstone and 1 major) for a hands-on learning experience from the industrial experts of top notch companies with 2 live sessions guidance.t-Time Python Developer at our Hyderabad-based organization, you will be a key player in designing and implementing scalable and efficient solutions using Python, with a focus on Flask and Django frameworks. Working closely with our development team, you will contribute to the delivery of high-quality software that meets the needs of our clients.",
+      openPositions: "Part Time",
+      link: "https://www.linkedin.com/jobs/view/3777298526/?alternateChannel=search&refId=0ujnMUxlrEA%2BYWn7%2B6az6Q%3D%3D&trackingId=rlLWKWc7sC3ENR%2FDdWsdSw%3D%3D",
   },
 ];
 
@@ -128,9 +156,9 @@ const createJobListingCards = () => {
       openPositions.classList.add("open-positions");
 
       if (job.openPositions == 1) {
-        openPositions.innerHTML = `${job.openPositions} open position`;
+        openPositions.innerHTML = `${job.openPositions} `;
       } else {
-        openPositions.innerHTML = `${job.openPositions} open positions`;
+        openPositions.innerHTML = `${job.openPositions} `;
       }
 
       jobCard.appendChild(image);
