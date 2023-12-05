41654af94d60bf6bf8723b483a58d6c90b4ed70d
purushotham5 <purushothamkandheri@gmail.com>
2023-12-05 21:00:31 +0530

    ae

--- job-portal.js
+++ job-portal.js
@@ -30,7 +30,8 @@ const jobs = [
     openPositions: "Fresher",
     link:
       "https://www.linkedin.com/jobs/view/software-engineer-fresher-at-covai-industrials-recruitment-3751442332/?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic&originalSubdomain=in",
-  },  {
+  },
+  {
     title: "Python Developer",
     image: "project-manager.svg",
     skills: "Skills:Python,SQL",
@@ -39,7 +40,8 @@ const jobs = [
     openPositions: "Fresher",
     link:
       "https://www.simplyhired.co.in/job/daKacOiPFIA11R3aJhnFZTDpdC4xn4VIeO13so0YAeih8Q8SqRprIQ?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic",
-  }, {
+  },
+  {
     title: "HTML / XML Programmer",
     image: "product-manager.svg",
     skills: "Skills:HTML,XML",
@@ -49,7 +51,6 @@ const jobs = [
     link:
       "https://freshersjobsalert.com/career/fresher-html-xml-programmer-jobs-in-tirupati-andhra-pradesh?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic",
   },
- 
 
   {
     title: "Work From Home/Computer Operator work",
@@ -71,7 +72,8 @@ const jobs = [
     openPositions: "Fresher",
     link:
       "https://alliance.wd3.myworkdayjobs.com/en-US/nissanjobs/job/software-engineer-i_r00151957-1",
-  },  {
+  },
+  {
     title: "JAVA Developer Internship",
     image: "sales-representative.svg",
     skills:
@@ -81,7 +83,8 @@ const jobs = [
     openPositions: "Internship",
     link:
       "https://www.linkedin.com/jobs/view/3667474965/?alternateChannel=search&refId=b4ed72a2-b021-499a-bb8f-fbfc95cdf26a&trackingId=lVFpV%2BduQj2bf0WdCNtTkw%3D%3D",
-  },  {
+  },
+  {
     title: "Java Programmer - Fresher",
     image: "marketing-manager.svg",
     skills:
@@ -91,7 +94,8 @@ const jobs = [
     openPositions: "Fresher",
     link:
       "https://www.simplyhired.co.in/job/6Zqg2_aeB8jIneXhXyyRH3T4lLV-LHTVefHgUuGVl1iNI3f0yH3j9g?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic",
-  }, {
+  },
+  {
     title: "Web Development Intern",
     image: "product-manager.svg",
     skills: "Skills:-HTML,CSS,JS (Basics)",
@@ -100,7 +104,7 @@ const jobs = [
     openPositions: "Internship",
     link:
       "https://internshala.com/internship/detail/web-development-part-time-job-internship-at-chennai-bangalore-in-dhanwantri-charity-trust1701398118",
-  }, 
+  },
   {
     title: "Product Management Intern    ",
     image: "sales-representative.svg",
@@ -111,7 +115,8 @@ const jobs = [
     openPositions: "Internship",
     link:
       "https://www.linkedin.com/jobs/view/3773061191/?alternateChannel=search&refId=R6bi8F4Df4U%2BOQMQhycVig%3D%3D&trackingId=SAhvQuqgb9YnbG%2B%2BSfGQdg%3D%3D#HYM",
-  },  {
+  },
+  {
     title: "Winter Training and Internship with Projects",
     image: "software-engineer.svg",
     skills:
@@ -121,7 +126,8 @@ const jobs = [
     openPositions: "Internship",
     link:
       "https://www.linkedin.com/jobs/view/3777298526/?alternateChannel=search&refId=0ujnMUxlrEA%2BYWn7%2B6az6Q%3D%3D&trackingId=rlLWKWc7sC3ENR%2FDdWsdSw%3D%3D",
-  },{
+  },
+  {
     title: "Web Development Intern",
     image: "product-manager.svg",
     skills: "Skills:-HTML,CSS,JS (Basics)",
@@ -130,7 +136,8 @@ const jobs = [
     openPositions: "Internship",
     link:
       "https://in.bebee.com/job/20231203-6667c09e1be5b6fb6d858e3872fa896c?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic",
-  },{
+  },
+  {
     title: "Python Development Intern    ",
     image: "sales-representative.svg",
     skills: "Skills: Web development SQL Python HTML5",
@@ -139,7 +146,8 @@ const jobs = [
     openPositions: "Internship",
     link:
       "https://www.simplyhired.co.in/job/ZubxhXG3HWoW5sAvrwNgeP5NSlBtMKVVihZp2Uldva_kj2JN0m9Mcw?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic",
-  },  {
+  },
+  {
     title: "Python Development Intern",
     image: "software-engineer.svg",
     skills: "Skills:-Python,MySql",
@@ -148,23 +156,36 @@ const jobs = [
     openPositions: "Internship",
     link:
       "https://www.linkedin.com/jobs/view/3777298526/?alternateChannel=search&refId=0ujnMUxlrEA%2BYWn7%2B6az6Q%3D%3D&trackingId=rlLWKWc7sC3ENR%2FDdWsdSw%3D%3D",
-  },{
+  },
+  {
     title: "Software Engineer I    ",
     image: "product-manager.svg",
-    skills: "Skills:-Comply with standards, version controls, and product release procedures.",
+    skills:
+      "Skills:-Comply with standards, version controls, and product release procedures.",
     details:
       "This position is responsible for enhancing and maintaining existing software products as well as developing new products.  The ideal candidate has a strong desire for excellence, with confidence in their expertise, and is a creative problem solver.",
-    openPositions: "Internship",
+    openPositions: "full time",
     link:
       "https://boards.greenhouse.io/fortra/jobs/4303268006?gh_src=61ca785e6us",
-  }, {
+  },
+  {
     title: "Associate Product Manager, University Graduate 2024    ",
     image: "software-engineer.svg",
     skills:
       "Skills:-Internship or Teaching Assistant experience in software development or a related field, Marketing, Research Skills, Team Work ",
     details:
       "Currently enrolled in a Bachelor's or Master's degree in Computer Science or a related field",
-    openPositions: "Internship",
+    openPositions: "full time",
+    link:
+      "https://www.google.com/about/careers/applications/jobs/results/100297584867713734-associate-product-manager-university-graduate-2024",
+  },
+  {
+    title: "Tata Communications Software & Application Development  ",
+    image: "sales-representative.svg",
+    skills: "Skills:-Good Knowledge on coding/testing environment,Team Work ",
+    details:
+      "The role is responsible for software development /testing/deployment/debugging process. This is an operational role that may seek appropriate level of guidance and advice to ensure delivery of quality outcomes.",
+    openPositions: "full time",
     link:
       "https://www.google.com/about/careers/applications/jobs/results/100297584867713734-associate-product-manager-university-graduate-2024",
   },
