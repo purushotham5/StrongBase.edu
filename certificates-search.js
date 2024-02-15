958a9dd143a5ae59b1f9ba3a43eef0fd23719399
Purushotham Kandheri <87255292+purushotham5@users.noreply.github.com>
2024-02-16 00:18:53 +0530

    kiua

--- certificates-search.js
+++ certificates-search.js
@@ -751,6 +751,7 @@ pyi01150146:'PYI01150146.pdf',
 pyi01150148:'PYI01150148.pdf',
 pyi01150149:'PYI01150149.pdf',
 pyi01150151:'PYI01150151.pdf',
+pyi01150152:'PYI01150152.pdf',
 
 
 // PYTHON course complition certificates 
