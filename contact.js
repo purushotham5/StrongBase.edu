39cb154d9b6ed250da122693bea5836f9bad18d8
Purushotham Kandheri <87255292+purushotham5@users.noreply.github.com>
2024-02-23 13:55:42 +0530

    eqe

--- contact.js
+++ contact.js
@@ -1,27 +1,3 @@
-// function sendMail() {
-//     var params = {
-//       name: document.getElementById("name").value,
-//       email: document.getElementById("email").value,
-//       message: document.getElementById("message").value,
-//     };
-
-//     const serviceID = "service_yntigur";
-//     const templateID = "template_igoxysg";
-
-//       emailjs.send(serviceID, templateID, params)
-//       .then(res=>{
-//           document.getElementById("name").value = "";
-//           document.getElementById("email").value = "";
-//           document.getElementById("message").value = "";
-//           console.log(res);
-//           alert("We Got Your Message! Our Team Will Get Back To You Soon")
-
-//       })
-//       .catch(err=>console.log(err));
-
-//   }
-
-
 const btn = document.getElementById('button');
 
 document.getElementById('form')
