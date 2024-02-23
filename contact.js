7ba6517ddacc89f32c52f83ab92754c477cc5291
Purushotham Kandheri <87255292+purushotham5@users.noreply.github.com>
2024-02-23 13:14:44 +0530

    mail

--- contact.js
+++ contact.js
@@ -4,10 +4,10 @@
 //       email: document.getElementById("email").value,
 //       message: document.getElementById("message").value,
 //     };
-  
+
 //     const serviceID = "service_yntigur";
 //     const templateID = "template_igoxysg";
-  
+
 //       emailjs.send(serviceID, templateID, params)
 //       .then(res=>{
 //           document.getElementById("name").value = "";
@@ -15,30 +15,30 @@
 //           document.getElementById("message").value = "";
 //           console.log(res);
 //           alert("We Got Your Message! Our Team Will Get Back To You Soon")
-  
+
 //       })
 //       .catch(err=>console.log(err));
-  
+
 //   }
 
 
 const btn = document.getElementById('button');
 
 document.getElementById('form')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
 
-   btn.value = 'Sending...';
+    btn.value = 'Sending...';
 
-   const serviceID = 'service_yntigur';
-   const templateID = 'template_igoxysg';
+    const serviceID = 'service_hzht0vr';
+    const templateID = 'template_cfo98ai';
 
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-      btn.value = 'Send Email';
-      alert('Sent!');
-    }, (err) => {
-      btn.value = 'Send Email';
-      alert(JSON.stringify(err));
-    });
-});
+    emailjs.sendForm(serviceID, templateID, this)
+      .then(() => {
+        btn.value = 'Send Email';
+        alert('Sent!');
+      }, (err) => {
+        btn.value = 'Send Email';
+        alert(JSON.stringify(err));
+      });
+  });
